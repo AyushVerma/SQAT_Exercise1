f1acc59863159837ff095398c14075917610362d
somename <someemail>
2016-09-22 16:14:43 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -13,7 +13,7 @@ public class TestBowling {
 	@Test
 	public void testFrameScore() throws BowlingException {
 		
-		Frame f = new Frame(10,1);
+		Frame f = new Frame(10,0);
 		int score = f.score();
 		assertTrue(score==10);
 	}
