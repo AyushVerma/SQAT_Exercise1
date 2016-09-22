b5000276eb8d183db492d4130be61ebca4c679bb
somename <someemail>
2016-09-22 15:34:27 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -2,9 +2,12 @@ public class Frame {
 	private int firstThrow;
 	private int secondThrow;
 	
-	public Frame(int firstThrow, int secondThrow){
+	public Frame(int firstThrow, int secondThrow) throws BowlingException{
 		this.firstThrow = firstThrow;
 		this.secondThrow = secondThrow;
+		if(firstThrow >10 || firstThrow+secondThrow>10) {
+			throw new BowlingException();
+		}
 	}
 	
 	public int getFirstThrow() {
@@ -17,8 +20,14 @@ public class Frame {
 
 	//returns the score of a single frame
 	public int score(){
-		//to be implemented
-		return 0;
+		
+		int score = 0;
+		if(firstThrow == 10)
+			score = 10;
+		else 
+			score = firstThrow +secondThrow;
+		
+		return score;
 	}
 
 	//returns whether the frame is a strike or not
